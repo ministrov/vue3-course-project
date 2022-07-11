900e78982c39572f8e1a0f67d10f07a147f8d9f2
ministrov <61511030+ministrov@users.noreply.github.com>
2022-07-11 14:47:11 +0300

    Did basics with App component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="addLike">Likes</button>
+    <button @click="addDislike">Dislikes</button>
+  </div>
+
+  <div>Cout like: <strong>{{ likes }}</strong></div>
+  <div>Cout dislikes: <strong>{{ dislikes }}</strong></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes++
+    },
+    addDislike() {
+      this.dislikes++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
